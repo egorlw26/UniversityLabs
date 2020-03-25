a820fc6d950b181019ffa01d50a71e57bc6773e3
egorlw26 <egorlw26@gmail.com>
2020-03-25 23:19:47 +0200

    Machine Learning lab1 first step

---
+++ 4Course/Semester2/MachineLearning/Lab1/DatasetFuncs.py
@@ -0,0 +1,47 @@
+import numpy as np
+import random
+import math
+from math import sqrt
+from math import pi
+from math import exp
+import matplotlib.pyplot as plt
+
+def createRowsForDataset(dataset, xrange, yrange, label):
+    for i in range(10):
+        x = np.random.rand() * (xrange[1] - xrange[0]) + xrange[0]
+        y = np.random.rand() * (yrange[1] - yrange[0]) + yrange[0]
+        dataset.append([x, y, label])
+
+def createDataset():
+    dataset = []
+    dataset.append([3.39, 2.3, 0])
+    dataset.append([3.11, 1.78, 0])
+    dataset.append([1.34, 3.36, 0])
+    dataset.append([3.58, 4.67, 0])
+    dataset.append([2.28, 2.86, 0])
+    dataset.append([7.42, 4.69, 1])
+    dataset.append([5.74, 3.53, 1])
+    dataset.append([9.17, 2.51, 1])
+    dataset.append([7.79, 3.42, 1])
+    dataset.append([7.93, 0.79, 1])
+    createRowsForDataset(dataset, [15, 20], [0, 5], 2)
+    createRowsForDataset(dataset, [10, 15], [2, 5], 3)
+    createRowsForDataset(dataset, [0, 20], [5, 10], 4)
+    return np.array(dataset)
+
+def separateByClass(dataset):
+    separated = {}
+    for i in range(len(dataset)):
+        vector = dataset[i]
+        class_value = vector[-1]
+        if(class_value not in separated):
+            separated[class_value] = list()
+        separated[class_value].append(vector)
+    return separated
+
+def plotDataset(dataset):
+    separated = separateByClass(dataset)
+    for i in range(len(separated)):
+        plt.scatter(
+        [x[0] for x in separated[i]],
+        [x[1] for x in separated[i]])
---
+++ 4Course/Semester2/MachineLearning/Lab1/NaiveBayes.py
@@ -0,0 +1,71 @@
+import random
+import math
+from math import sqrt
+from math import pi
+from math import exp
+import matplotlib.pyplot as plt
+from sklearn.naive_bayes import GaussianNB
+import numpy as np
+from DatasetFuncs import *
+
+
+def mean(numbers):
+    return sum(numbers)/float(len(numbers))
+
+def stdev(numbers):
+    avg = mean(numbers)
+    variance = sum([(x-avg/2)**2 for x in numbers]) / float(len(numbers))
+    return sqrt(variance)
+
+def summarizeDataset(dataset):
+    summaries = [(mean(column), stdev(column), len(column)) for column in zip(*dataset)]
+    del(summaries[-1])
+    return summaries
+
+def summarizeByClass(dataset):
+    separated = separateByClass(dataset)
+    summaries = {}
+    for class_value, rows in separated.items():
+        summaries[class_value] = summarizeDataset(rows)
+    return summaries
+
+def calculatePropability(x, mean, stdev):
+    exponent = exp(-((x-mean)**2 / (2 *  stdev**2)))
+    return (1 / (sqrt(2*pi)*stdev)) * exponent
+
+def calculateClassPropabilities(summaries, row):
+    total_rows = sum([summaries[label][0][2] for label in summaries])
+    probalities = {}
+    for class_value, class_summaries in summaries.items():
+        probalities[class_value] = summaries[class_value][0][2]/float(total_rows)
+        for i in range(len(class_summaries)):
+            mean, stdev, count = class_summaries[i]
+            probalities[class_value] *= calculatePropability(row[i], mean, stdev)
+    return probalities
+
+def predict(dataset, row):
+    summaries = summarizeByClass(dataset)
+    probabilities = calculateClassPropabilities(summaries, row)
+    label = max(probabilities, key=probabilities.get)
+    print("Label:", label)
+    row.append(label)
+    dataset = np.append(dataset,row)
+
+def outOfBoxPredict(dataset, row):
+    sk_bc = GaussianNB()
+    sk_bc.fit(dataset[:,0:2], dataset[:,2])
+    predictions = sk_bc.predict_proba([row])[0]
+    print(np.argmax(predictions))
+
+if __name__ == '__main__':
+    dataset = createDataset()
+    predict(dataset, [1, 1])
+    predict(dataset, [10, 6])
+
+    outOfBoxPredict(dataset, [10, 6])
+
+    plotDataset(dataset)
+
+
+
+    plt.show()
