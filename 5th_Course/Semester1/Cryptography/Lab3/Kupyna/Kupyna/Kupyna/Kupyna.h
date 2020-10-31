091f588e942e324e8a4f4d788ad7cb5ca5c9e6cf
egorlw26 <egorlw26@gmail.com>
2020-10-31 10:36:33 +0200

    Kupyna done

---
+++ 5th_Course/Semester1/Cryptography/Lab3/Kupyna/Kupyna/Kupyna/Kupyna.h
@@ -0,0 +1,31 @@
+#pragma once
+#include <vector>
+#include <string>
+
+class Kupyna
+{
+private:
+	std::vector<std::vector<uint8_t>> m_state;
+	size_t m_nbytes;
+	size_t m_hash_bits;
+	int m_columns;
+	int m_rounds;
+
+	void SubBytes(std::vector<std::vector<uint8_t>>& state);
+	void ShiftBytes(std::vector<std::vector<uint8_t>>& state);
+	void MixColumns(std::vector<std::vector<uint8_t>>& state);
+	void AddRoundConstantP(std::vector<std::vector<uint8_t>>& state, const int round);
+	void AddRoundConstantQ(std::vector<std::vector<uint8_t>>& state, const int round);
+	void P(std::vector<std::vector<uint8_t>>& state);
+	void Q(std::vector<std::vector<uint8_t>>& state);
+	void Pad(std::string& msg);
+	void Digest(const std::string& msg);
+	void OutputTransform();
+	int KupynaInit(const size_t hash_nbits);
+	void KupynaHash(const size_t hash_bits, const std::string& input);
+
+public:
+	std::string showResult();
+	Kupyna(const size_t hash_bits, const std::string& input);
+};
+
---
+++ 5th_Course/Semester1/Cryptography/Lab3/Kupyna/Kupyna/Kupyna/main.cpp
@@ -0,0 +1,13 @@
+#include "Kupyna.h"
+#include <iostream>
+#include <string>
+
+int main()
+{
+	Kupyna k(256, "");
+	
+
+	std::cout << k.showResult() << std::endl;
+
+	return 0;
+}
